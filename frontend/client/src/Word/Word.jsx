2c5a65e0c06590ae9d4e8df88084530cf8ad8448
Luciano Sammartino <lucianosammar@gmal.com>
2025-08-13 15:08:29 -0300

    letter & word state

--- frontend/client/src/Word/Word.jsx
+++ frontend/client/src/Word/Word.jsx
@@ -1,14 +1,27 @@
 import styles from './Word.module.css';
+import { useState } from 'react';
 
 function Word() {
+
+
+const [actualLetter, setActualLetter] = useState("1")
+const [actualWord, setActualWord] = useState("arbol")
+
+
     return (
     <main className={styles.word}>
-
-        <div className={styles.letter}></div>
-        <div className={styles.letter}></div>
-        <div className={styles.letter}></div>
-        <div className={styles.letter}></div>
-        <div className={styles.letter}></div>
+    
+        <div className={ `${styles.letter} ${actualLetter === "1" ? styles.active : ""}`} 
+        onClick={ () => setActualLetter("1")} >{actualWord[0]} 
+        </div>
+        <div className={ `${styles.letter} ${actualLetter === "2" ? styles.active : ""}`}
+        onClick={ () => setActualLetter("2")}>{actualWord[1]}</div>
+        <div className={ `${styles.letter} ${actualLetter === "3" ? styles.active : ""}`}
+        onClick={ () => setActualLetter("3")}>{actualWord[2]}</div>
+        <div className={ `${styles.letter} ${actualLetter === "4" ? styles.active : ""}`}
+        onClick={ () => setActualLetter("4")}>{actualWord[3]}</div>
+        <div className={ `${styles.letter} ${actualLetter === "5" ? styles.active : ""}`}
+        onClick={ () => setActualLetter("5")}>{actualWord[4]}</div>
 
     </main>
 )
