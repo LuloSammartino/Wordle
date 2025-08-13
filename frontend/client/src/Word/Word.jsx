ee928f6cb0d23e15d63b0ec391a0b03faca52a53
Luciano Sammartino <lucianosammar@gmal.com>
2025-08-13 16:45:09 -0300

    letras inputs, onKey bug

--- frontend/client/src/Word/Word.jsx
+++ frontend/client/src/Word/Word.jsx
@@ -1,27 +1,55 @@
 import styles from './Word.module.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 function Word() {
 
-
-const [actualLetter, setActualLetter] = useState("1")
+const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef()];
+const [actualLetter, setActualLetter] = useState(1)
 const [actualWord, setActualWord] = useState("arbol")
 
+const handlechange = (e) => {
+    if(!e.target.value){
+        
+        actualLetter != 1 ? setActualLetter(actualLetter - 1) (inputRefs[actualLetter - 2 ].current.focus()) : ""
+        
+    } else {
+        actualLetter < 5 ? setActualLetter(actualLetter + 1) (inputRefs[actualLetter].current.focus())  : ""
+    }
+}
 
     return (
     <main className={styles.word}>
     
-        <div className={ `${styles.letter} ${actualLetter === "1" ? styles.active : ""}`} 
-        onClick={ () => setActualLetter("1")} >{actualWord[0]} 
-        </div>
-        <div className={ `${styles.letter} ${actualLetter === "2" ? styles.active : ""}`}
-        onClick={ () => setActualLetter("2")}>{actualWord[1]}</div>
-        <div className={ `${styles.letter} ${actualLetter === "3" ? styles.active : ""}`}
-        onClick={ () => setActualLetter("3")}>{actualWord[2]}</div>
-        <div className={ `${styles.letter} ${actualLetter === "4" ? styles.active : ""}`}
-        onClick={ () => setActualLetter("4")}>{actualWord[3]}</div>
-        <div className={ `${styles.letter} ${actualLetter === "5" ? styles.active : ""}`}
-        onClick={ () => setActualLetter("5")}>{actualWord[4]}</div>
+        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 1 ? styles.active : ""}`} 
+        onClick={ () => setActualLetter(1)}
+        onKeyUp={(e) => handlechange(e)}
+        ref={inputRefs[0]}> 
+        </input>
+
+        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 2 ? styles.active : ""}`}
+        onClick={ () => setActualLetter(2)}
+        onKeyUp={(e) => handlechange(e)}
+        ref={inputRefs[1]}>
+        </input>
+
+        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 3 ? styles.active : ""}`}
+        onClick={ () => setActualLetter(3)}
+        onKeyUp={(e) => handlechange(e)}
+        ref={inputRefs[2]}>
+        </input>
+
+        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 4 ? styles.active : ""}`}
+        onClick={ () => setActualLetter(4)}
+        onKeyUp={(e) => handlechange(e)}
+        ref={inputRefs[3]}>
+        </input>
+
+        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 5 ? styles.active : ""}`}
+        onClick={ () => setActualLetter(5)}
+        onKeyUp={(e) => handlechange(e)}
+        ref={inputRefs[4]}>
+
+        </input>
 
     </main>
 )
