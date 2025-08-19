bbc7a115a4670780412d19ce998fe852eb7fc0a2
Luciano Sammartino <lucianosammar@gmal.com>
2025-08-19 10:13:12 -0300

    inputs.map

--- frontend/client/src/Word/Word.jsx
+++ frontend/client/src/Word/Word.jsx
@@ -1,55 +1,45 @@
 import styles from './Word.module.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 function Word() {
 
-const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef()];
-const [actualLetter, setActualLetter] = useState(1)
-const [actualWord, setActualWord] = useState("arbol")
+useEffect(() => {
+    inputRefs[actualLetter].current.focus();
+})
 
+const inputRefs = [useRef(), useRef(), useRef(), useRef(), useRef()];
+const [actualLetter, setActualLetter] = useState(0)
+
+
+///////////////////////////SOLUCIONAR PROBLEMA ONKEYUP///////////////////////////////////////////////////////////
 const handlechange = (e) => {
-    if(!e.target.value){
+    console.log(e)
+    if(!e.target.value) {
         
-        actualLetter != 1 ? setActualLetter(actualLetter - 1) (inputRefs[actualLetter - 2 ].current.focus()) : ""
+        actualLetter != 0 ? setActualLetter(actualLetter - 1) : ""
         
     } else {
-        actualLetter < 5 ? setActualLetter(actualLetter + 1) (inputRefs[actualLetter].current.focus())  : ""
+        actualLetter < 4 ? setActualLetter(actualLetter + 1)  : ""
     }
 }
 
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
     return (
     <main className={styles.word}>
-    
-        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 1 ? styles.active : ""}`} 
-        onClick={ () => setActualLetter(1)}
-        onKeyUp={(e) => handlechange(e)}
-        ref={inputRefs[0]}> 
-        </input>
 
-        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 2 ? styles.active : ""}`}
-        onClick={ () => setActualLetter(2)}
-        onKeyUp={(e) => handlechange(e)}
-        ref={inputRefs[1]}>
-        </input>
+    {inputRefs.map((ref, i) =>    
+        <input  key={i}
+                maxLength="1" 
+                className={ `${styles.letter} ${actualLetter === i ? styles.active : ""}`}
+                onClick={ () => setActualLetter(i)}
+                onChange={(e) => handlechange(e)}
+                ref={ref}>
+        </input>)}
+        
 
-        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 3 ? styles.active : ""}`}
-        onClick={ () => setActualLetter(3)}
-        onKeyUp={(e) => handlechange(e)}
-        ref={inputRefs[2]}>
-        </input>
-
-        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 4 ? styles.active : ""}`}
-        onClick={ () => setActualLetter(4)}
-        onKeyUp={(e) => handlechange(e)}
-        ref={inputRefs[3]}>
-        </input>
-
-        <input maxLength="1" className={ `${styles.letter} ${actualLetter === 5 ? styles.active : ""}`}
-        onClick={ () => setActualLetter(5)}
-        onKeyUp={(e) => handlechange(e)}
-        ref={inputRefs[4]}>
-
-        </input>
 
     </main>
 )
