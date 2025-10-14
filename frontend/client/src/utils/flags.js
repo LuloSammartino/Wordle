10b4034ff7fdc4679b725033eb4e6a0a725a3999
Luciano Sammartino <lucianosammar@gmail.com>
2025-10-14 20:17:19 -0300

    solve render

--- frontend/client/src/utils/flags.js
+++ frontend/client/src/utils/flags.js
@@ -1,12 +1,12 @@
 import Spain from '../assets/Spain.png'
-import Usa from '../assets/usa.png'
-import France from '../assets/france.png'
-import Germany from '../assets/germany.png'
-import Italy from '../assets/italy.png'
-import Netherlands from '../assets/netherlands.png'
-import Brazil from '../assets/brazil.png'
-import SaudiArabia from '../assets/saudiarab.png'
-import Russia from '../assets/russia.png'
+import Usa from '../assets/Usa.png'
+import France from '../assets/France.png'
+import Germany from '../assets/Germany.png'
+import Italy from '../assets/Italy.png'
+import Netherlands from '../assets/Netherlands.png'
+import Brazil from '../assets/Brazil.png'
+import SaudiArabia from '../assets/SaudiArab.png'
+import Russia from '../assets/Russia.png'
 
 const flags = [
     { code: "es", img: Spain },
