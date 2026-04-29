93015f72878f14c11a4c0a209d098a3c75f4f89a
Luciano Sammartino <lucianosammar@gmail.com>
2026-04-29 13:06:56 -0300

    delete flags except ES & EN

--- frontend/client/src/utils/flags.js
+++ frontend/client/src/utils/flags.js
@@ -1,23 +1,9 @@
 import Spain from '../assets/flags/Spain.webp'
 import Usa from '../assets/flags/Usa.webp'
-import France from '../assets/flags/France.webp'
-import Germany from '../assets/flags/Germany.webp'
-import Italy from '../assets/flags/Italy.webp'
-import Netherlands from '../assets/flags/Netherlands.webp'
-import Brazil from '../assets/flags/Brazil.webp'
-import SaudiArabia from '../assets/flags/SaudiArab.webp'
-import Russia from '../assets/flags/Russia.webp'
 
 const flags = [
     { code: "es", img: Spain },
-    { code: "en", img: Usa },
-    { code: "fr", img: France },
-    { code: "de", img: Germany },
-    { code: "it", img: Italy },
-    { code: "nl", img: Netherlands },
-    { code: "pt", img: Brazil },
-    { code: "ar", img: SaudiArabia },
-    { code: "ru", img: Russia }
+    { code: "en", img: Usa }
 ];
 
 export default flags;
